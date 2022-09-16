326bc19022617691459c14719565e79071223294
wojijiwo <dev@wojijiwo.me>
2022-09-16 16:18:39 +0800

    Update metric parameters

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.a010e83662e26ffdf221b7f1e76faa76.js"
+  "./precache-manifest.d3b69148250c0946b1621cefe038a963.js"
 );
 
 self.addEventListener('message', (event) => {
